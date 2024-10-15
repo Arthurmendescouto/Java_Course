9e2d93d5e1cdfbb2b6c559dade465717ebd608e1
ArthurMendes <153393682+Arthurmendescouto@users.noreply.github.com>
2024-10-15 12:33:16 -0300

    novo exercicio

--- ex1/Estoque/src/Main.java
+++ ex1/Estoque/src/Main.java
@@ -11,7 +11,32 @@ public class Main {
         Scanner sc=new Scanner(System.in);
 
         Product product=new Product();
+        System.out.println("Enter product data: ");
+        System.out.print("Name:");
+        product.name=sc.nextLine();
+        System.out.println("Price: ");
+        product.price=sc.nextDouble();
+        System.out.println("Quantity in stock: ");
+        product.quantity=sc.nextInt();
 
+        System.out.println();
+        System.out.println("Product data: "+product);
 
+        System.out.println();
+        System.out.println("Enter the number of products to be added in stock: ");
+        int quantity=sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Update data: "+product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be added in stock: ");
+        quantity= sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Update data: "+product);
+        sc.close();
         }
     }
