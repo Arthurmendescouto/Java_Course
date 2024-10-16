df56f4b4ef2e83e05b7874dbc48a062c95ee9d1a
ArthurMendes <153393682+Arthurmendescouto@users.noreply.github.com>
2024-10-16 01:27:50 -0300

    primeira parte

--- ex2/ex_area/src/entities/formulas.java
+++ ex2/ex_area/src/entities/formulas.java
@@ -1,4 +1,19 @@
 package entities;
 
 public class formulas {
+    public String texto="";
+    public double height;
+    public double width;
+
+
+    public double area(){
+        return height*width;
+    }
+    public double perimetro(){
+     return
+    }
+    public String toString(){
+         return texto
+                 +"AREA= " + area();
+    }
 }
